72216b37288230ec3e0e6d05b26a1ee1b80dc2f1
LannyRivero <lannyrivero288@gmail.com>
2025-02-16 15:00:34 +0100

    feat: Created test testCanFastAttack_KnightIsAwake, testCanFastAttack_KnightIsAsSleep for method canFastAttack, but it fails due to the method not being implemented

---
+++ src/test/java/dev/lanny/annalys/AnnalysInfiltrationTest.java
@@ -0,0 +1,25 @@
+package dev.lanny.annalys;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class AnnalysInfiltrationTest {
+    @Test
+    @DisplayName("Validamos que el caballero esta despierto y no se puede atacar")
+    void testCantFastAttack_KnightIsAwake() {
+        boolean knightIsAwake = true;
+        assertFalse(AnnalynsInfiltration.canFastAttack(knightIsAwake));
+    }
+
+    @Test
+    @DisplayName("Validamos que el caballero esta dormido y se puede atacar")
+    void testCantFastAttack_KnightIsSleeping() {
+        boolean knightIsAwake = false;
+        assertTrue(AnnalynsInfiltration.canFastAttack(knightIsAwake));
+    }
+
+}
